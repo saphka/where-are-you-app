517556345e448e8299697fb0c2135b168c80f65d
saphka <alex.loginov@pm.me>
2022-04-17 10:32:24 +0300

    Initial commit

---
+++ WhereAreYou/WhereAreYouApp.swift
@@ -0,0 +1,18 @@
+//
+//  WhereAreYouApp.swift
+//  WhereAreYou
+//
+//  Created by Alex Loginov on 17.04.2022.
+//
+//
+
+import SwiftUI
+
+@main
+struct WhereAreYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
